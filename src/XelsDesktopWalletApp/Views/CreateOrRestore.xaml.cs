ba662c7553f6089e32174cea78fcd2d74a72490a
fazle rabbi <fazlerabbi2008@yahoo.com>
2021-07-13 17:13:09 +0600

    close window activate

--- src/XelsDesktopWalletApp/Views/CreateOrRestore.xaml.cs
+++ src/XelsDesktopWalletApp/Views/CreateOrRestore.xaml.cs
@@ -33,7 +33,7 @@ namespace XelsDesktopWalletApp.Views
         {
             MainWindow mw = new MainWindow();
             mw.Show();
-           // this.Close();
+           this.Close();
         }
     }
 }
