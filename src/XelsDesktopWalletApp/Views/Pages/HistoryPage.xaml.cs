f9ca0b8f727eeca609698fe897b57eef8cd8078b
fazle rabbi <fazlerabbi2008@yahoo.com>
2021-08-02 23:05:25 +0600

    history page refresh timer

--- src/XelsDesktopWalletApp/Views/Pages/HistoryPage.xaml.cs
+++ src/XelsDesktopWalletApp/Views/Pages/HistoryPage.xaml.cs
@@ -169,10 +169,10 @@ namespace XelsDesktopWalletApp.Views.Pages
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            //DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            //dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            //dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
-            //dispatcherTimer.Start();
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+            dispatcherTimer.Start();
         }
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
