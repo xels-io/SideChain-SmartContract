3e93188e30cec5ec589978f6e9e70112335c9d17
nahid9300 <princeneo58@gmail.com>
2021-12-03 17:03:31 +0600

    Custom modal added on XelsCCDesktopWalletApp

---
+++ src/XelsCCDesktopWalletApp/Views/Pages/Modals/AccountCreatedUserControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XelsCCDesktopWalletApp.Views.Pages.Modals
+{
+    /// <summary>
+    /// Interaction logic for AccountCreatedUserControl.xaml
+    /// </summary>
+    public partial class AccountCreatedUserControl : UserControl
+    {
+        public AccountCreatedUserControl(string message)
+        {
+            InitializeComponent();
+            this.Message_Modal.Text = message;
+        }
+        private void Button_Close_Modal(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+            CreateOrRestore parentWindow = (CreateOrRestore)Window.GetWindow(this);
+            parentWindow.Visibility = Visibility.Collapsed;
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+    }
+}
--- src/XelsCCDesktopWalletApp/Views/Pages/ReceivePages/BelsPage.xaml.cs
+++ src/XelsCCDesktopWalletApp/Views/Pages/ReceivePages/BelsPage.xaml.cs
@@ -18,6 +18,7 @@ using XelsCCDesktopWalletApp.Common;
 using XelsCCDesktopWalletApp.Models;
 using XelsCCDesktopWalletApp.Models.CommonModels;
 using XelsCCDesktopWalletApp.Models.SmartContractModels;
+using XelsCCDesktopWalletApp.Views.Pages.Modals;
 
 namespace XelsCCDesktopWalletApp.Views.Pages.ReceivePages
 {
@@ -69,11 +70,13 @@ namespace XelsCCDesktopWalletApp.Views.Pages.ReceivePages
             if (addressString !="")
             {
                 Clipboard.SetText(addressString);
-                MessageBox.Show("Address Copied Successfully :- " + addressString);
+                //MessageBox.Show("Address Copied Successfully :- " + addressString);
+                this.Bels_Receive_Page.Children.Add(new DisplayMessageUserControl("Address Copied Successfully : " + this.textBoxTextToQr.Text.ToString()));
             }
             else
             {
-                MessageBox.Show("Data Not Found!.");
+                //MessageBox.Show("Data Not Found!.");
+               this.Bels_Receive_Page.Children.Add(new DisplayErrorMessageUserControl("Data Not Found!."));
             }
         }
          
@@ -94,7 +97,8 @@ namespace XelsCCDesktopWalletApp.Views.Pages.ReceivePages
                 }
                 else
                 {
-                    MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+                    //MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+                    this.Bels_Receive_Page.Children.Add(new DisplayErrorMessageUserControl("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase));
                 }
 
                 return content;
