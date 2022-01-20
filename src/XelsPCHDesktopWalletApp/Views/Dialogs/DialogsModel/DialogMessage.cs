9ebf50f42da59dac31c76a23a72e0c5414df2390
nahid9300 <princeneo58@gmail.com>
2022-01-20 13:03:42 +0600

    Stable DialogHost of Material UI in PCH. Also Singleton added.

---
+++ src/XelsPCHDesktopWalletApp/Views/Dialogs/DialogsModel/DialogMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XelsPCHDesktopWalletApp.Views.Dialogs.DialogsModel
+
+{
+    public class DialogMessage
+    {
+        private string _message = "";
+        private string _title = "";
+
+        public string Message { get => _message; set => _message = value; }
+        public string Title { get => _title; set => _title = value; }
+
+    }
+}
