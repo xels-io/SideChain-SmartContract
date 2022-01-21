7c4a090df6905cdf199253a5639d4263b4a77b27
nahid9300 <princeneo58@gmail.com>
2022-01-21 18:32:33 +0600

    Remove redundent code of Dialog Base class on PCH

--- src/XelsPCHDesktopWalletApp/Views/Dialogs/DialogsModel/DialogMessage.cs
+++ src/XelsPCHDesktopWalletApp/Views/Dialogs/DialogsModel/DialogMessage.cs
@@ -7,11 +7,14 @@ namespace XelsPCHDesktopWalletApp.Views.Dialogs.DialogsModel
 {
     public class DialogMessage
     {
-        private string _message = "";
-        private string _title = "";
+        //private string _message = "";
+        //private string _title = "";
 
-        public string Message { get => _message; set => _message = value; }
-        public string Title { get => _title; set => _title = value; }
+        //public string Message { get => _message; set => _message = value; }
+        //public string Title { get => _title; set => _title = value; }
+
+        public string Message{ get; set; }
+        public string Title{ get; set; }
 
     }
 }
