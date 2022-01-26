b7e24ebc4aca7e09428d465f4e037238cf81f4b4
nahid9300 <princeneo58@gmail.com>
2022-01-26 19:52:22 +0600

    Configured MaterialUI's Dialoghost on XLC.

---
+++ src/XelsXLCDesktopWalletApp/Views/Dialogs/DialogsModel/DialogMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XelsXLCDesktopWalletApp.Views.Dialogs.DialogsModel
+{
+    public class DialogMessage
+    {
+        public string Message { get; set; }
+        public string Title { get; set; }
+    }
+}
